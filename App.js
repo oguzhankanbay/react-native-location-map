84f1b8e8cc67643392b07556e027da3871a07a96
oguzhankanbay <kanbayoguzhan@gmail.com>
2024-03-07 01:51:51 +0300

    react native follow instant location

--- App.js
+++ App.js
@@ -1,11 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker, Circle } from 'react-native-maps';
+import { Button, StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 60.1,
+    longitude: -80.2,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
+  const [circleRadius, setCircleRadius] = useState(100);
+
+  const userLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('izin reddedildi!');
+    } else {
+      let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+      setMapRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      });
+      console.log(location.coords.latitude, location.coords.longitude);
+    }
+  };
+
+  useEffect(() => {
+    userLocation();
+
+    const locationWatchId = Location.watchPositionAsync(
+      {
+        enableHighAccuracy: true,
+        distanceInterval: 10,
+      },
+      (location) => {
+        setMapRegion({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        });
+      }
+    );
+
+
+    return () => {
+      locationWatchId.remove();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} region={mapRegion}>
+        <Marker coordinate={{ latitude: mapRegion.latitude, longitude: mapRegion.longitude }} title='marker' />
+        <Circle
+          center={{ latitude: mapRegion.latitude, longitude: mapRegion.longitude }}
+          radius={circleRadius}
+          fillColor="rgba(0, 0, 110, 0.3)"
+        />
+      </MapView>
+      <Button title='Get Location' onPress={userLocation} />
     </View>
   );
 }
@@ -13,8 +70,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
